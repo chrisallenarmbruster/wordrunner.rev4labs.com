c1a614b360ce6d7b164e185d010c675c046e4938
Chris Armbruster <chris@armbrustermail.com>
2023-04-10 18:13:21 -0500

    Created, abstracted and integrated UI class.

--- js/ui.js
+++ js/ui.js
@@ -1,19 +1,18 @@
 export class UI {
   constructor(container) {
     this.initialUiSetup(container)
-    this.uiState = {
-      curRow: 0,
-      curCol: 0,
-      board: [
-        ["", "", "", "", ""],
-        ["", "", "", "", ""],
-        ["", "", "", "", ""],
-        ["", "", "", "", ""],
-        ["", "", "", "", ""],
-        ["", "", "", "", ""],
-      ],
-      busy: false,
-    }
+    this.audioSetup()
+    this.curRow = 0
+    this.curCol = 0
+    this.board = [
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+    ]
+    this.busy = false
   }
 
   initialUiSetup(container) {
@@ -41,6 +40,70 @@ export class UI {
     container.appendChild(modalContainer)
   }
 
+  audioSetup() {
+    this.clickAudio = new Audio("./audio/click.mp3")
+    this.compAudio = new Audio("./audio/comp.mp3")
+    this.successAudio = new Audio("./audio/fight.mp3")
+    this.failAudio = new Audio("./audio/regret.mp3")
+    this.invalidAudio = new Audio("./audio/invalid.mp3")
+    this.ratchetAudio = new Audio("./audio/ratchet.mp3")
+  }
+
+  reset() {
+    this.curRow = 0
+    this.curCol = 0
+    this.board = [
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+    ]
+    this.busy = false
+    ENTER.classList.remove("gameOver")
+    ENTER.textContent = "ENTER"
+    header.className = "header"
+    header.textContent = "wordBrunner"
+    this.flipAndResetTiles()
+    for (let letter of "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")) {
+      let key = document.getElementById(letter)
+      key.className = "key"
+    }
+  }
+
+  iterateTiles(callback) {
+    for (let row = 0; row < 6; row++) {
+      for (let col = 0; col < 5; col++) {
+        callback(document.getElementById(`tile-${row}-${col}`))
+      }
+    }
+  }
+
+  flipAndResetTiles() {
+    this.clickAudio.pause()
+    this.ratchetAudio.play().catch((error) => {
+      /*do nothing - it's just audio*/
+    })
+
+    setTimeout(() => {
+      this.iterateTiles((tile) => {
+        tile.classList.remove("tileHit", "tileClose", "tileMiss")
+        tile.innerHTML = '<span class="tileWaterMark">B</span>'
+      })
+    }, 500)
+
+    setTimeout(() => {
+      this.iterateTiles((tile) => {
+        tile.classList.remove("reset")
+      })
+    }, 1000)
+
+    this.iterateTiles((tile) => {
+      tile.classList.add("reset")
+    })
+  }
+
   drawTile(container, row, col, value = "") {
     const tile = document.createElement("div")
     tile.id = `tile-${row}-${col}`
@@ -64,12 +127,9 @@ export class UI {
 
   drawKey(key) {
     const keyButton = document.createElement("button")
-    keyButton.id = key === "⌫" ? "Backspace" : key === "ENTER" ? "Enter" : key
+    keyButton.id = key === "⌫" ? "BACKSPACE" : key === "ENTER" ? "ENTER" : key
     keyButton.className = key === " " ? "keySpacer" : "key"
     keyButton.textContent = key
-    keyButton.addEventListener("click", () =>
-      this.handleKeyButtonClick(keyButton.id)
-    )
     return keyButton
   }
 
@@ -107,26 +167,125 @@ export class UI {
     this.drawKeyboardRow(keyboardGrid, 3, keys[2])
   }
 
-  handleKeyButtonClick(key) {
-    console.log(key)
-    if (!this.uiState.busy) {
-      clickAudio.currentTime = 0
-      clickAudio.play().catch((error) => {
+  appendLetter(letter) {
+    if (this.curCol < 5 && this.curRow < 6) {
+      const tile = document.getElementById(`tile-${this.curRow}-${this.curCol}`)
+      tile.textContent = letter
+      this.board[this.curRow][this.curCol] = letter
+      this.curCol++
+    }
+  }
+
+  deleteLetter() {
+    if (this.curCol > 0) {
+      this.curCol--
+      const tile = document.getElementById(`tile-${this.curRow}-${this.curCol}`)
+      tile.innerHTML = '<span class="tileWaterMark">B</span>'
+      this.board[this.curRow][this.curCol] = ""
+    }
+  }
+
+  displayMessage(message, time = 3500) {
+    header.className = "message"
+    header.textContent = message
+    setTimeout(() => {
+      header.className = "header"
+      header.textContent = "wordBrunner"
+    }, time)
+  }
+
+  updateKeyboard(letterStatus) {
+    for (let [letter, status] of Object.entries(letterStatus)) {
+      let key = document.getElementById(letter)
+      key.classList.add(
+        status === "G"
+          ? "tileHit"
+          : status === "Y"
+          ? "tileClose"
+          : status === "R"
+          ? "tileMiss"
+          : "key"
+      )
+    }
+  }
+
+  async revealGuess(guessStatus) {
+    return new Promise(async (resolve, reject) => {
+      this.busy = true
+      let gArr = guessStatus
+      this.compAudio.play().catch((error) => {
         /*do nothing - it's just audio*/
       })
-      if (game.gameState === "PLAYING") {
-        if (key === "Backspace") {
-          deleteLetter()
-        } else if (key === "Enter") {
-          checkRow()
-        } else {
-          appendLetter(key)
-        }
-      } else {
-        if (key === "Enter") {
-          resetGame()
-        }
-      }
+      let word = gArr[this.curRow]
+      let interval = setInterval(() => this.scrambleEffect(), 30)
+      await new Promise((resolve, reject) => {
+        setTimeout(resolve, 1000)
+      })
+      clearInterval(interval)
+      this.colorTiles(word)
+      this.busy = false
+      resolve()
+    })
+  }
+
+  scrambleEffect() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for (let col = 0; col < 5; col++) {
+      let tile = document.getElementById(`tile-${this.curRow}-${col}`)
+      tile.textContent = letters[Math.floor(Math.random() * 26)]
     }
+  }
+
+  colorTiles(word) {
+    for (let [idx, letter] of word.entries()) {
+      let tile = document.getElementById(`tile-${this.curRow}-${idx}`)
+      tile.textContent = word[idx]["letter"]
+      tile.classList.add(
+        letter.status === "G"
+          ? "tileHit"
+          : letter.status === "Y"
+          ? "tileClose"
+          : "tileMiss"
+      )
+    }
+  }
+
+  showModal(title = "Title", content = ["lorem ipsum"], gameState) {
+    let modalContent = document.createElement("div")
+    modalContent.id = "modalContent"
+    modalContent.className = "modalContent"
+
+    let closeButton = document.createElement("span")
+    closeButton.id = "closeButton"
+    closeButton.className = "close"
+    closeButton.textContent = `x`
+    closeButton.addEventListener("click", () => {
+      modalContainer.style.display = "none"
+      this.successAudio.pause()
+      this.successAudio.currentTime = 0
+      this.failAudio.pause()
+      this.failAudio.currentTime = 0
+      if (gameState !== "PLAYING") {
+        ENTER.classList.add("gameOver")
+        ENTER.textContent = "RESET"
+      }
+    })
+    modalContent.appendChild(closeButton)
+
+    let modalTitle = document.createElement("h4")
+    modalTitle.className = "modalTitle"
+    modalTitle.textContent = title
+    modalContent.appendChild(modalTitle)
+
+    for (let item of content) {
+      let modalContentItem = document.createElement("p")
+      modalContentItem.className = "modalContentItem"
+      modalContentItem.innerHTML = item
+      modalContent.appendChild(modalContentItem)
+    }
+
+    modalContainer.replaceChildren()
+    modalContainer.appendChild(modalContent)
+    modalContainer.style.display = "block"
   }
 }
